e44e6ef27bc72bf017692ba29da7cc4bf2ba1987
KorazX <88162403+KorazX@users.noreply.github.com>
2021-11-08 20:20:50 +0100

    Update Program.cs
    Napisane jeszcze raz od zera. Tylko znacznie lepiej

--- Program.cs
+++ Program.cs
@@ -1,36 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using SFML;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
-using SFML.Audio;
+using System;
 
-namespace JPWP_Projekt {
+class Program {
+    public static RenderWindow okienko;
     
-    class Program {
-        ///Podstawowa klasa pelniaca role przelacznika pomiedzy Menu gry, a sama gra
-        static void Main(string[] args) {
 
-            MenuGlowne a = new MenuGlowne();
-            Gra b = new Gra();
-            a.Dzialaj();
-            while (true)
-            {
-                if (a.stan == false)
-                {
-                    if (a.wylacz) break; ///Wylaczenie aplikacji (wybranie 'Wyjście' w menu)
-                    b.Dzialaj(a.tryb);
-                }
-                if (b.stan == false)
-                {
-                    a.Dzialaj();
-                }
-            }
-            
-        }
-
+    public static void Main()
+    {
+        okienko = new RenderWindow(new VideoMode(1024, 768), "Gra", Styles.Fullscreen);
+        okienko.SetMouseCursorVisible(false);
+        MenuGlowne menuglowne = new MenuGlowne();
     }
 }
